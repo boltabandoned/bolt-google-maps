1959ff4f7665ec9a7b45d1e46191b62260156c12
Alan Smithee <alan.smithee@example.com>
2015-02-13 14:33:00 +0100

    move to non-inline scripts

--- Extension.php
+++ Extension.php
@@ -24,10 +24,10 @@ class Extension extends \Bolt\BaseExtension
             if (isset($this->config['defaultzoom'])) {
                 $defaultzoom = $this->config['defaultzoom'];
             } else {
-                $defaultzoom = 1;
+                $defaultzoom = 14;
             }
             
-            $snippet = '<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script><script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerwithlabel/src/markerwithlabel.js"></script><script>var mapstyles = '.$mapstyles.'; function mapInit(){Array.prototype.forEach.call(document.getElementsByClassName("map-canvas"),function(e){var t=JSON.parse(e.dataset.mapobj);var n={disableDefaultUI:true,styles:mapstyles,scrollwheel: false,navigationControl: false,mapTypeControl: false,scaleControl: false,draggable: false,disableDefaultUI:true};var r=new google.maps.Map(e,n);var s=new google.maps.LatLngBounds;var o=new google.maps.InfoWindow;for(i=0;i<t.length;i++){if(t[i].icon){var u=new MarkerWithLabel({labelAnchor:new google.maps.Point(8,22),icon: " ",labelContent: "<i class=\"fa fa-"+t[i].icon+" fa-2x\" style=\"color:"+t[i].color+";\"></i>",labelStyle: {color: t[i].color}, position:new google.maps.LatLng(t[i].latitude,t[i].longitude),map:r});}else{var u=new google.maps.Marker({position:new google.maps.LatLng(t[i].latitude,t[i].longitude),map:r});} s.extend(u.position);google.maps.event.addListener(u,"click",function(e,n){return function(){o.setContent("<div class=\"mapContent\">"+t[n].html+"</div>");o.open(r,e)}}(u,i))}r.fitBounds(s); window.onresize = function() { r.fitBounds(s); }; if(t.length==1){var e=google.maps.event.addListener(r,"idle",function(){r.setZoom('.$defaultzoom.');google.maps.event.removeListener(e)})}})}google.maps.event.addDomListener(window,"load",mapInit())</script>';
+            $snippet = '<script defer="defer" src="https://maps.googleapis.com/maps/api/js?v=3.exp&callback=initializeMap"></script><script defer="defer" src="/extensions/local/intendit/gmaps/assets/gmaps.js"></script><script>var mapstyles = '.$mapstyles.', defaultzoom = '.$defaultzoom.';</script>';
             
             
               $this->addTwigFunction('gmaps', 'gmapsExt');
