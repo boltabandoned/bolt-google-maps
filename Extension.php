32d54dbc2379769cef0ccb2da2dc9e80ba20d880
Svante Richter <svante.richter@intendit.se>
2015-08-27 14:37:46 +0200

    Fix bug where some browsers would get
    mapstyles and defaultzoom undefined errors.

--- Extension.php
+++ Extension.php
@@ -27,7 +27,7 @@ class Extension extends \Bolt\BaseExtension
                 $defaultzoom = 14;
             }
             
-            $snippet = '<script>var mapstyles = '.$mapstyles.', defaultzoom = '.$defaultzoom.';</script><style>.map-canvas{height:50vh;}</style>';
+            $snippet = '<script>window.mapstyles = '.$mapstyles.', window.defaultzoom = '.$defaultzoom.';</script><style>.map-canvas{height:50vh;}</style>';
             if (!$this->app['config']->get('general/disable_script_injecting')){
                 $this->addJavascript('/assets/gmaps.js');
                 $snippet .='<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>';
