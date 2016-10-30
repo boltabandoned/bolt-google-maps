f2f1c928830b043cf94a38cc4d8e84d128fe7697
Alan Smithee <alan.smithee@example.com>
2016-10-30 20:00:33 +0100

    MIT licensing

--- src/gmapsExtension.php
+++ src/gmapsExtension.php
@@ -9,28 +9,10 @@ use Bolt\Asset\Snippet\Snippet;
 use Bolt\Asset\File\JavaScript;
 use Bolt\Asset\File\Stylesheet;
 
-/**
- * Gmaps extension for bolt
+/* Copyright (C) 2015-2016 Alan Smithee
  *
- * Copyright (C) 2015-2016 Alan Smithee
- *
- * This program is free software: you can redistribute it under the terms of 
- * the GNU General Public License or GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the Licenses,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author    Alan Smithee <alan.smithee@example.com>
- * @copyright Copyright (c) 2015-2016, Alan Smithee
- * @license   http://opensource.org/licenses/GPL-3.0 GNU Public License 3.0
- * @license   http://opensource.org/licenses/LGPL-3.0 GNU Lesser General Public License 3.0
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
  */
 class gmapsExtension extends SimpleExtension
 {
